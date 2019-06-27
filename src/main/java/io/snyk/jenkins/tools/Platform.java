66fc1f305803ae84c986ddc6a22e5c5b43dea6a3
pavel <36902936+pavel-github@users.noreply.github.com>
2019-06-28 00:07:40 +0200

    Add LINUX_ALPINE platform definition

--- src/main/java/io/snyk/jenkins/tools/Platform.java
+++ src/main/java/io/snyk/jenkins/tools/Platform.java
@@ -1,6 +1,7 @@
 package io.snyk.jenkins.tools;
 
 import javax.annotation.Nonnull;
+import java.nio.file.Paths;
 import java.util.Locale;
 import java.util.Map;
 
@@ -12,6 +13,7 @@ import hudson.model.Node;
  */
 public enum Platform {
   LINUX("node", "npm", "bin", "snyk-linux", "snyk-to-html-linux"),
+  LINUX_ALPINE("node", "npm", "bin", "snyk-alpine", "snyk-to-html-alpine"),
   MAC_OS("node", "npm", "bin", "snyk-macos", "snyk-to-html-macos"),
   WINDOWS("node.exe", "npm.cmd", "", "snyk-win.exe", "snyk-to-html-win.exe");
 
@@ -58,7 +60,7 @@ public enum Platform {
   private static Platform detect(@Nonnull Map<Object, Object> systemProperties) throws ToolDetectionException {
     String arch = ((String) systemProperties.get("os.name")).toLowerCase(Locale.ENGLISH);
     if (arch.contains("linux")) {
-      return LINUX;
+      return Paths.get("/etc/alpine-release").toFile().exists() ? LINUX_ALPINE : LINUX;
     } else if (arch.contains("mac os x") || arch.contains("darwin") || arch.contains("osx")) {
       return MAC_OS;
     } else if (arch.contains("windows")) {
