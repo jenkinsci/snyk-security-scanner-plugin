69dd75bd8c228a2d7c0408a78f2c27d5fd0cf518
Jahed Ahmed <jahed@snyk.io>
2021-07-12 19:32:54 +0100

    feat: support multiple reports in a single build

--- src/main/java/io/snyk/jenkins/SnykToHTML.java
+++ src/main/java/io/snyk/jenkins/SnykToHTML.java
@@ -3,61 +3,68 @@ package io.snyk.jenkins;
 import hudson.EnvVars;
 import hudson.FilePath;
 import hudson.Launcher;
-import hudson.Util;
-import hudson.model.TaskListener;
 import hudson.util.ArgumentListBuilder;
 import io.snyk.jenkins.tools.SnykInstallation;
 import io.snyk.jenkins.transform.ReportConverter;
 import jenkins.model.Jenkins;
 
 import java.io.IOException;
+import java.time.Instant;
 
 import static io.snyk.jenkins.config.SnykConstants.SNYK_REPORT_HTML;
 import static io.snyk.jenkins.config.SnykConstants.SNYK_TEST_REPORT_JSON;
 import static java.nio.charset.StandardCharsets.UTF_8;
 
 public class SnykToHTML {
-  public static void generateReport(
+  public static FilePath generateReport(
     SnykContext context,
     SnykInstallation installation
-  ) throws IOException, InterruptedException {
-    TaskListener log = context.getTaskListener();
-    FilePath workspace = context.getWorkspace();
-    Launcher launcher = context.getLauncher();
-    EnvVars env = context.getEnvVars();
-
-    ArgumentListBuilder args = new ArgumentListBuilder();
-
-    FilePath snykReportJson = workspace.child(SNYK_TEST_REPORT_JSON);
-    if (!snykReportJson.exists()) {
-      log.getLogger().println("Snyk report json doesn't exist");
-      return;
-    }
-
-    workspace.child(SNYK_REPORT_HTML).write("", UTF_8.name());
-
-    args.add(installation.getReportExecutable(launcher));
-    args.add("-i", SNYK_TEST_REPORT_JSON, "-o", SNYK_REPORT_HTML);
+  ) {
     try {
+      FilePath workspace = context.getWorkspace();
+      Launcher launcher = context.getLauncher();
+      EnvVars env = context.getEnvVars();
+
+      FilePath snykReportJson = workspace.child(SNYK_TEST_REPORT_JSON);
+      if (!snykReportJson.exists()) {
+        throw new RuntimeException("Snyk Report JSON does not exist.");
+      }
+
+      FilePath reportPath = workspace.child(getURLSafeDateTime() + "_" + SNYK_REPORT_HTML);
+      reportPath.write("", UTF_8.name());
+
+      ArgumentListBuilder args = new ArgumentListBuilder()
+        .add(installation.getReportExecutable(launcher))
+        .add("-i", SNYK_TEST_REPORT_JSON);
+
       int exitCode = launcher.launch()
         .cmds(args)
         .envs(env)
+        .stdout(reportPath.write())
         .quiet(true)
         .pwd(workspace)
         .join();
-      boolean success = exitCode == 0;
-      if (!success) {
-        log.getLogger().println("Generating Snyk html report was not successful");
+
+      if (exitCode != 0) {
+        throw new RuntimeException("Exited with non-zero exit code. (Exit Code: " + exitCode + ")");
       }
-      String reportWithInlineCSS = workspace.child(SNYK_REPORT_HTML).readToString();
-      String finalHtmlReport = ReportConverter.getInstance().modifyHeadSection(
-        reportWithInlineCSS,
+
+      String reportContents = ReportConverter.getInstance().modifyHeadSection(
+        reportPath.readToString(),
         Jenkins.get().servletContext.getContextPath()
       );
-      workspace.child(workspace.getName() + "_" + SNYK_REPORT_HTML).write(finalHtmlReport, UTF_8.name());
-    } catch (IOException ex) {
-      Util.displayIOException(ex, log);
-      ex.printStackTrace(log.fatalError("Snyk-to-Html command execution failed"));
+
+      reportPath.write(reportContents, UTF_8.name());
+
+      return reportPath;
+    } catch (IOException | InterruptedException | RuntimeException ex) {
+      throw new RuntimeException("Failed to generate report.", ex);
     }
+  }
+
+  private static String getURLSafeDateTime() {
+    return Instant.now().toString()
+      .replaceAll(":", "-")
+      .replaceAll("\\.", "-");
   }
 }
