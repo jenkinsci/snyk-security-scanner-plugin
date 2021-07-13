57a84296e8fa647e06e58abdb5c1cbed07bb4bd2
Jahed Ahmed <jahed@snyk.io>
2021-07-13 18:28:48 +0100

    feat: monitor only after test flow has completed

--- src/main/java/io/snyk/jenkins/SnykStepFlow.java
+++ src/main/java/io/snyk/jenkins/SnykStepFlow.java
@@ -17,21 +17,34 @@ import java.util.function.Supplier;
 
 public class SnykStepFlow {
 
-  public static void perform(SnykConfig config, Supplier<SnykContext> contextSupplier) throws SnykIssueException, SnykErrorException {
+  public static void perform(SnykConfig config, Supplier<SnykContext> contextSupplier)
+  throws SnykIssueException, SnykErrorException {
     int testExitCode = 0;
     Exception cause = null;
     SnykContext context = null;
 
     try {
       context = contextSupplier.get();
-      testExitCode = SnykStepFlow.scan(context, config);
+
+      SnykInstallation installation = SnykInstallation.install(
+        context,
+        config.getSnykInstallation()
+      );
+
+      context.getEnvVars().put("SNYK_TOKEN", SnykApiToken.getToken(context, config.getSnykTokenId()));
+
+      testExitCode = SnykStepFlow.testProject(context, config, installation);
+
+      if (config.isMonitorProjectOnBuild()) {
+        SnykMonitor.monitorProject(context, config, installation);
+      }
     } catch (IOException | InterruptedException | RuntimeException ex) {
       if (context != null) {
         TaskListener listener = context.getTaskListener();
         if (ex instanceof IOException) {
           Util.displayIOException((IOException) ex, listener);
         }
-        ex.printStackTrace(listener.fatalError("Snyk command execution failed"));
+        ex.printStackTrace(listener.fatalError("Snyk Security scan failed."));
       }
       cause = ex;
     }
@@ -44,26 +57,13 @@ public class SnykStepFlow {
     }
   }
 
-  private static int scan(SnykContext context, SnykConfig config)
+  private static int testProject(SnykContext context, SnykConfig config, SnykInstallation installation)
   throws IOException, InterruptedException {
-    SnykInstallation installation = SnykInstallation.install(
-      context,
-      config.getSnykInstallation()
-    );
-
-    context.getEnvVars().put("SNYK_TOKEN", SnykApiToken.getToken(context, config.getSnykTokenId()));
-
-    int testExitCode = SnykTest.testProject(context, config, installation);
-
-    if (config.isMonitorProjectOnBuild()) {
-      SnykMonitor.monitorProject(context, config, installation);
-    }
-
+    int exitCode = SnykTest.testProject(context, config, installation);
     FilePath report = SnykToHTML.generateReport(context, installation);
     archiveReport(context, report);
     addSidebarLink(context);
-
-    return testExitCode;
+    return exitCode;
   }
 
   private static void archiveReport(SnykContext context, FilePath report) throws IOException, InterruptedException {
