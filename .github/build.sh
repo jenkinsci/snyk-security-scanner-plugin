e3e87b40b8e8a1c956844079a4a896fae508ec5f
Bastian Doetsch <bastian.doetsch@snyk.io>
2022-07-04 03:32:41 -0500

    chore: refactor build to use jenkins, install python

--- .github/build.sh
+++ .github/build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-set -ex
+set -e
 SCRIPT_RELATIVE_DIR=$(dirname "${BASH_SOURCE[0]}")
 pushd "$SCRIPT_RELATIVE_DIR"/.. || exit 1
-cp -rp ~/.m2 ./.m2
-docker build -t jenkins-snyk -f .github/Dockerfile "$PWD"
-rm -rf ./.m2
+  # use local .m2 repo to speed up build
+  rsync -crlDv ~/.m2 ./.m2
+  docker build -t jenkins-snyk -f .github/Dockerfile .
+  rm -rf ./.m2
 popd || exit
