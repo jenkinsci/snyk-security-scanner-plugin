1e6f7834b84a9ed0de8f580c526d99d6f18e0eb6
Kaspar Lyngsie <kaspar.moss@snyk.io>
2025-05-16 13:04:15 +0200

    fix: upgrading infrastructure to Java 17

--- .github/run.sh
+++ .github/run.sh
@@ -6,7 +6,7 @@ HOST="http://$(echo "$DOCKER_HOST" | cut -f3 -d "/" | cut -f1 -d":"):8080"
 echo "Please connect to IP address $HOST"
 # --platform linux/amd64 is required for M1 Macs
 # to persist data:
-  # create a volume using docker `volume create <VOLUME_NAME>`
-  # mount the volume via docker run command using `-v <VOLUME_NAME>:/var/jenkins_home`
+# create a volume using docker `volume create <VOLUME_NAME>`
+# mount the volume via docker run command using `-v <VOLUME_NAME>:/var/jenkins_home`
 docker run --rm --platform linux/amd64 -p 8080:8080 -p 50000:50000 --name=jenkins-snyk jenkins-snyk
 popd || exit
--- Jenkinsfile
+++ Jenkinsfile
@@ -2,8 +2,8 @@
 
 /* `buildPlugin` step provided by: https://github.com/jenkins-infra/pipeline-library */
 buildPlugin(configurations: [
-  [ platform: "linux", jdk: "11" ],
-  [ platform: "windows", jdk: "11" ],
-  [ platform: "linux", jdk: "11", jenkins: "2.401.3", javaLabel: 11 ],
-  [ platform: "windows", jdk: "11", jenkins: "2.401.3", javaLabel: 11 ]
+  [ platform: "linux", jdk: "17" ],
+  [ platform: "windows", jdk: "17" ],
+  [ platform: "linux", jdk: "17", jenkins: "2.479.1", javaLabel: 17 ],
+  [ platform: "windows", jdk: "17", jenkins: "2.479.1", javaLabel: 17 ]
 ])
